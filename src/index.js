3b459de8c78a4ba02d1e765c7adafbdf2d07d8e8
Mahadi <manakamedia@gmail.com>
2025-05-16 00:54:27 +0200

    Added functionality for the select dropdown
    1. Added timezones for each city in the dropdown
    2. When a user selects a city, a new city name is created using the timezone by splitting the timezone name in two & getting the second value only & replace any underscores in the second value with a space
    3. The default cities data is replaced with real-time data of the city selected by the user (city name, current date, current time)

--- src/index.js
+++ src/index.js
@@ -20,3 +20,22 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cities = document.querySelector("#cities");
+  cities.innerHTML = ` <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
+let selectCities = document.querySelector("#city");
+selectCities.addEventListener("change", updateCity);
