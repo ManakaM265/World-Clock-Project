5129768180d2cb04ca185074d13043df1af467ac
Mahadi <manakamedia@gmail.com>
2025-05-15 20:57:01 +0200

    Replaced placeholder time with real-time data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 let nairobi = document.querySelector("#nairobi");
 let nairobiDate = nairobi.querySelector(".date");
+let nairobiTime = nairobi.querySelector(".time");
+let nairobiCurrentTime = moment().tz("Africa/Nairobi");
 nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
+nairobiTime.innerHTML = nairobiCurrentTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
 
 let london = document.querySelector("#london");
 let londonDate = london.querySelector(".date");
+let londonTime = london.querySelector(".time");
+let londonCurrentTime = moment().tz("Europe/London");
 londonDate.innerHTML = moment().format("MMMM Do YYYY");
+londonTime.innerHTML = londonCurrentTime.format("h:mm:ss [<small>]A[</small>]");
