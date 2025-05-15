f1486f73e336de4b947c17a7227c6a17c30cd656
Mahadi <manakamedia@gmail.com>
2025-05-15 21:08:39 +0200

    Setup time to update every second
    Time displays like a ticking numerical clock

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-let nairobi = document.querySelector("#nairobi");
-let nairobiDate = nairobi.querySelector(".date");
-let nairobiTime = nairobi.querySelector(".time");
-let nairobiCurrentTime = moment().tz("Africa/Nairobi");
-nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
-nairobiTime.innerHTML = nairobiCurrentTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+function updateTime() {
+  let nairobi = document.querySelector("#nairobi");
+  let nairobiDate = nairobi.querySelector(".date");
+  let nairobiTime = nairobi.querySelector(".time");
+  let nairobiCurrentTime = moment().tz("Africa/Nairobi");
+  nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
+  nairobiTime.innerHTML = nairobiCurrentTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-let london = document.querySelector("#london");
-let londonDate = london.querySelector(".date");
-let londonTime = london.querySelector(".time");
-let londonCurrentTime = moment().tz("Europe/London");
-londonDate.innerHTML = moment().format("MMMM Do YYYY");
-londonTime.innerHTML = londonCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+  let london = document.querySelector("#london");
+  let londonDate = london.querySelector(".date");
+  let londonTime = london.querySelector(".time");
+  let londonCurrentTime = moment().tz("Europe/London");
+  londonDate.innerHTML = moment().format("MMMM Do YYYY");
+  londonTime.innerHTML = londonCurrentTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
