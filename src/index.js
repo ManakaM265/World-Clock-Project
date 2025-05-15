7686c6f4a93adb251603de4a4e65288b994e3718
Mahadi <manakamedia@gmail.com>
2025-05-15 20:21:28 +0200

    Replaced placeholder date with real-time data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+let nairobi = document.querySelector("#nairobi");
+let nairobiDate = nairobi.querySelector(".date");
+nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
 
+let london = document.querySelector("#london");
+let londonDate = london.querySelector(".date");
+londonDate.innerHTML = moment().format("MMMM Do YYYY");
